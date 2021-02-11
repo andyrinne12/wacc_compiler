f146acfbb709134aa02ad81f24247d9f836a1702
Cristina <icm19@ic.ac.uk>
2021-02-11 10:39:57 +0000

    [icm19] the exit statement must take an integer

--- src/front_end/AST/StatementAST/Exit.java
+++ src/front_end/AST/StatementAST/Exit.java
@@ -1,5 +1,25 @@
 package front_end.AST.StatementAST;
 
-public class Exit {
+import front_end.AST.ExpressionAST.ExpressionAST;
+import front_end.Visitor;
+import front_end.types.IDENTIFIER;
+import org.antlr.v4.runtime.ParserRuleContext;
 
+public class Exit extends Statement{
+  private ExpressionAST expression;
+
+  public Exit(ParserRuleContext ctx, ExpressionAST expression) {
+    super(ctx);
+    this.expression = expression;
+  }
+
+  @Override
+  public void check() {
+    IDENTIFIER ident = Visitor.ST.lookupAll("int");
+    expression.wasChecked();
+    if(!expression.getType().equals(ident.getType())) {
+      error("Exit code must me an integer");
+    }
+
+  }
 }
