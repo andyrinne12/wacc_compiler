5c673d5101f74371d0c85b7e632a5edf107f5048
Cristina <icm19@ic.ac.uk>
2021-02-11 11:27:07 +0000

    [icm19] verification of what can be freed done + if conditon type must be boolean

--- src/front_end/AST/StatementAST/If.java
+++ src/front_end/AST/StatementAST/If.java
@@ -1,5 +1,38 @@
 package front_end.AST.StatementAST;
 
-public class If {
+import front_end.AST.ExpressionAST.ExpressionAST;
+import front_end.SymbolTable;
+import front_end.Visitor;
+import front_end.types.IDENTIFIER;
+import org.antlr.v4.runtime.ParserRuleContext;
 
+public class If extends Statement{
+  private ExpressionAST expression;
+  private Statement then;
+  private SymbolTable thenST;
+  private Statement elseSt;
+  private SymbolTable elseST;
+
+  public If(ParserRuleContext ctx, ExpressionAST expression, Statement then, Statement elseSt, SymbolTable thenST, SymbolTable elseST) {
+    super(ctx);
+    this.expression = expression;
+    this.then = then;
+    this.elseSt = elseSt;
+    this.thenST = thenST;
+    this.elseST = elseST;
+  }
+
+  @Override
+  public void check() {
+    //assure the validity of the expression
+    expression.wasChecked();
+    IDENTIFIER ident = Visitor.ST.lookupAll("bool");
+    if(!expression.getType().equals(ident)) {
+      error("If condition type is not boolean");
+    }
+
+    //check that the statements are valid
+    then.wasChecked();
+    then.wasChecked();
+  }
 }
