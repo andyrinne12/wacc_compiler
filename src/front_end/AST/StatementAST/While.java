558c5f9946e5890bdd83c38a9cdd5c4bf690e444
Cristina <icm19@ic.ac.uk>
2021-02-11 12:19:40 +0000

    [icm19] check method for while statement implemented

--- src/front_end/AST/StatementAST/While.java
+++ src/front_end/AST/StatementAST/While.java
@@ -1,5 +1,32 @@
 package front_end.AST.StatementAST;
 
-public class While {
+import front_end.AST.ExpressionAST.ExpressionAST;
+import front_end.SymbolTable;
+import front_end.Visitor;
+import org.antlr.v4.runtime.ParserRuleContext;
 
+public class While extends Statement {
+  ExpressionAST expression;
+  Statement stat;
+  SymbolTable ST;
+
+  public While(ParserRuleContext ctx, ExpressionAST expression, Statement stat, SymbolTable ST) {
+    super(ctx);
+    this.expression = expression;
+    this.stat = stat;
+    this.ST = ST;
+  }
+
+  @Override
+  public void check() {
+    //check the validity of th expression
+    expression.wasChecked();
+
+    if(expression.getType().equals(Visitor.ST.lookupAll("bool"))) {
+      //check the validity of the statement
+      stat.wasChecked();
+    } else {
+      error("while condition is not boolean");
+    }
+  }
 }
