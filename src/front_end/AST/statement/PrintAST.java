d2d696ce219753c265f4cc5f366ba36916d5c020
Cristina <icm19@ic.ac.uk>
2021-03-02 19:23:45 +0000

    [icm19] assemble for print and println statements implemented

--- src/front_end/AST/statement/PrintAST.java
+++ src/front_end/AST/statement/PrintAST.java
@@ -1,6 +1,11 @@
 package front_end.AST.statement;
 
+import back_end.FunctionBody;
+import back_end.instructions.logical.MOV;
+import back_end.operands.registers.Register;
+import back_end.operands.registers.RegisterManager;
 import front_end.AST.expression.ExpressionAST;
+import java.util.List;
 import org.antlr.v4.runtime.ParserRuleContext;
 
 public class PrintAST extends StatementAST {
@@ -14,5 +19,10 @@ public class PrintAST extends StatementAST {
   @Override
   public void check() {
     expression.check();
+  }
+
+  @Override
+  public void assemble(FunctionBody body, List<Register> freeRegs) {
+    expression.assemble(body, freeRegs);
   }
 }
--- src/front_end/AST/statement/PrintlnAST.java
+++ src/front_end/AST/statement/PrintlnAST.java
@@ -1,6 +1,11 @@
 package front_end.AST.statement;
 
+import back_end.FunctionBody;
+import back_end.instructions.Condition;
+import back_end.instructions.branch.BL;
+import back_end.operands.registers.Register;
 import front_end.AST.expression.ExpressionAST;
+import java.util.List;
 import org.antlr.v4.runtime.ParserRuleContext;
 
 public class PrintlnAST extends StatementAST {
@@ -14,5 +19,11 @@ public class PrintlnAST extends StatementAST {
   @Override
   public void check() {
     expression.check();
+  }
+
+  @Override
+  public void assemble(FunctionBody body, List<Register> freeRegs) {
+    expression.assemble(body, freeRegs);
+    body.addInstr(new BL(Condition.LT, "p_print_ln"));
   }
 }
